085ec5eae1c73528c7a16ec3a2f64d6c2c7c0630
Shachar Oz <shachar.oz@prophecy.digital>
2021-06-15 08:44:04 +0200

    rubik cube animations (external example to building animator, animations and animation state behaviors

---
+++ Assets/Rubric Cube/BlocksController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksController : MonoBehaviour
+{
+    public Transform AllBlock;
+    public Transform TopParent;
+    public Transform LeftParent;
+
+    public List<Transform> Blocks;
+
+
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// reset the parent of all blocks to save the last position of it. 
+    /// called from the Animator state beavior
+    /// </summary>
+    private void ResetBlocks()
+    {
+        foreach (Transform t in Blocks)
+        {
+            t.SetParent(AllBlock);
+        }
+    }
+
+    public void SetHorizontalGroups()
+    {
+        //top
+        foreach (Transform t in Blocks)
+        {
+            if (Mathf.RoundToInt( t.localPosition.y) == 1)
+            {
+                t.parent = TopParent;
+            }
+        }
+    }
+
+    public void SetVerticalGroups()
+    {
+        //left
+        foreach (Transform t in Blocks)
+        {
+            if (Mathf.RoundToInt(t.localPosition.x) == 1)
+            {
+                t.parent = LeftParent;
+            }
+        }
+    }
+}
---
+++ Assets/Rubric Cube/OnAnimationEnd.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnAnimationEnd : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    //public UnityEvent OnEnded;
+    public string OnAnimationEndedFunctionName = "OnEnded";
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.SendMessage(OnAnimationEndedFunctionName);
+        //OnEnded.Invoke(); // doesnt work. cannot reference anything from the hirachy
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
