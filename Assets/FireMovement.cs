c259689819d5999925a5f6af5d987953db759137
Shachar Oz <shachar.oz@prophecy.digital>
2021-06-29 07:25:52 +0200

    fireball capability to mario

---
+++ Assets/FireMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMovement : MonoBehaviour
+{
+    public float Speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * Speed);
+    }
+}
---
+++ Assets/Fireballs/FireControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class FireControler : MonoBehaviour
+{
+    public KeyCode FireKey = KeyCode.Space;
+    public GameObject BulletPrefab;
+
+    void Start()
+    {
+        /*
+        Debug.Log("Before sleep "+ Time.time); //0
+        
+        //you cannot sleep the game thread
+        Thread.Sleep(5000);
+
+        Debug.Log("After sleep " + Time.time); //5
+        */
+
+    }
+
+    private float timeCountSinceLastSpawn = 0;
+    public int TimeGapBetweenAutomaticFire = 30; //Rate of Fire
+
+    void Update()
+    {
+        /*
+        if (Time.time - timeCountSinceLastSpawn >= TimeGapBetweenAutomaticFire)
+        {
+            timeCountSinceLastSpawn = Time.time;
+
+            CreateBullet();
+        }
+        */
+
+        //if user click
+        if (Input.GetKeyDown(FireKey))
+        {
+            CreateBullet();
+        }
+    }
+
+    private void CreateBullet()
+    {
+        Instantiate(BulletPrefab, transform.position, transform.rotation);
+    }
+}
