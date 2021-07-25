40ff0a3fcae12e4c9374cfc0a9658758a5077e87
Shachar Oz <shachar.oz@prophecy.digital>
2021-07-25 13:00:14 +0200

    mario extra content, 3d models, fireball

--- Assets/Fireballs/FireControler.cs
+++ Assets/Fireballs/FireControler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class FireControler : MonoBehaviour
@@ -8,33 +7,8 @@ public class FireControler : MonoBehaviour
     public KeyCode FireKey = KeyCode.Space;
     public GameObject BulletPrefab;
 
-    void Start()
-    {
-        /*
-        Debug.Log("Before sleep "+ Time.time); //0
-        
-        //you cannot sleep the game thread
-        Thread.Sleep(5000);
-
-        Debug.Log("After sleep " + Time.time); //5
-        */
-
-    }
-
-    private float timeCountSinceLastSpawn = 0;
-    public int TimeGapBetweenAutomaticFire = 30; //Rate of Fire
-
     void Update()
     {
-        /*
-        if (Time.time - timeCountSinceLastSpawn >= TimeGapBetweenAutomaticFire)
-        {
-            timeCountSinceLastSpawn = Time.time;
-
-            CreateBullet();
-        }
-        */
-
         //if user click
         if (Input.GetKeyDown(FireKey))
         {
@@ -47,3 +21,32 @@ public class FireControler : MonoBehaviour
         Instantiate(BulletPrefab, transform.position, transform.rotation);
     }
 }
+
+
+
+/*
+        if (Time.time - timeCountSinceLastSpawn >= TimeGapBetweenAutomaticFire)
+        {
+            timeCountSinceLastSpawn = Time.time;
+
+            CreateBullet();
+        }
+        */
+
+    /*
+     void Start()
+    {
+        /
+        Debug.Log("Before sleep "+ Time.time); //0
+        
+        //you cannot sleep the game thread
+        Thread.Sleep(5000);
+
+        Debug.Log("After sleep " + Time.time); //5
+        /
+
+    }
+
+    private float timeCountSinceLastSpawn = 0;
+public int TimeGapBetweenAutomaticFire = 30; //Rate of Fire
+*/
